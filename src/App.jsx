c964ee1b9cc54ef1a94d609a0929571a9d267932
awaisdoto <awaissaleem442@gmail.com>
2023-02-13 05:17:06 +0500

    I dont know bruh

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,17 @@
-import React from "react";
 import { createRoot } from "react-dom";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.age),
-    React.createElement("h2", {}, props.orgin),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, // we can also put null instead of empty object both of them mean essentially the same thing.
-    [
-      React.createElement("h1", {}, "PLEASE ADOPT ME !"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        age: 13,
-        orgin: "USA",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        age: 8,
-        orgin: "Rio",
-      }),
-      React.createElement(Pet, {
-        name: "Garfield",
-        animal: "Cat",
-        age: 4,
-        orgin: "Canada",
-      }),
-    ]
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" age="13" origin="USA" />
+      <Pet name="Pepper" animal="Bird" age="8" origin="Rio" />
+      <Pet name="Garfield" animal="Cat" age="4" origin="Pakistan" />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
